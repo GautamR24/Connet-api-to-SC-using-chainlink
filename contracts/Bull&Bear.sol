d2cb8c5f927c7da77036b662b600b74c13db56c5
GautamR24 <rawatgautam24@protonmail.com>
2022-11-11 17:30:50 +0530

    added the upkeep and perform keep functions

--- contracts/Bull&Bear.sol
+++ contracts/Bull&Bear.sol
@@ -7,30 +7,152 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+/*
+
+*importing chainlink contracts
+ */
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+
+
+contract BullBear is ERC721,ERC721Enumerable,ERC721URIStorage, KeeperCompatibleInterface, Ownable 
+{
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    uint256 public interval;
+    uint256 public lastTimeStamp;
 
-    constructor() ERC721("Bull&Bear", "BBTK") {}
+    AggregatorV3Interface public priceFeed;
+    int256 public currentPrice;
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    //ipfs URIs for the dynaminc nft graphics/metadata
+
+    string[] bullUrisIpfs = [
+        "https://ipfs.io/ipfs/QmRXyfi3oNZCubDxiVFre3kLZ8XeGt6pQsnAQRZ7akhSNs?filename=gamer_bull.json",
+        "https://ipfs.io/ipfs/QmRJVFeMrtYS2CUVUM2cHJpBV5aX2xurpnsfZxLTTQbiD3?filename=party_bull.json",
+        "https://ipfs.io/ipfs/QmdcURmN1kEEtKgnbkVJJ8hrmsSWHpZvLkRgsKKoiWvW9g?filename=simple_bull.json"
+    ];
+    string[] bearUrisIpfs = [
+        "https://ipfs.io/ipfs/Qmdx9Hx7FCDZGExyjLR6vYcnutUR8KhBZBnZfAPHiUommN?filename=beanie_bear.json",
+        "https://ipfs.io/ipfs/QmTVLyTSuiKGUEmb88BgXG3qNC8YgpHZiFbjHrXKH3QHEu?filename=coolio_bull.json",
+        "https://ipfs.io/ipfs/QmbKhBXVWmwrYsTPFYfroR2N7NAekAMxHUVg2CWks7i9qj?filename=simple_bear.json"
+    ];
+
+    event tokensUpdated(string marketTrend);
+
+    
+   
+
+    constructor(uint256 updateInterval, address _priceFeed)
+        ERC721("Bull&Bear", "BBTK")
+    {
+        //sets the keeper update interval
+        interval = updateInterval;
+        lastTimeStamp = block.timestamp;
+
+        /*
+         * set the price feed address to BTC/USD price Feed contract address : https://goerli.etherscan.io/address/0xA39434A63A52E749F02807ae27335515BA4b07F7
+         * or the mockPriced Contract
+         */
+
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        currentPrice = getLatestPrice();
+    }
+
+    function safeMint(address to) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        //defaults to gamer nft
+        string memory defaultUri = bullUrisIpfs[0];
+        _setTokenURI(tokenId, defaultUri);
     }
+
+
+    /**
+    below funtions need to overidden to work with chainlink */
+
+    function checkUpKeep(bytes calldata ) external view returns(bool upKeepNeeded, bytes memory ){
+        upKeepNeeded = (block.timestamp - lastTimeStamp) > interval;
+    }
+
+    function performUpKeep(bytes calldata) external {
+        if((block.timestamp - lastTimeStamp) > interval){
+            lastTimeStamp = block.timestamp;
+            int latestPrice = getLatestPrice();
+
+            if(latestPrice == currentPrice){
+                return;
+            }
+            if(latestPrice < currentPrice){
+                //Bear
+                updateAllTokenUris("bear");
+
+            }else{
+                //Bull
+                updateAllTokenUris("bull");
+            }
+            currentPrice = latestPrice;
+        }
+    }
+
+    function getLatestPrice() public view returns(int){
+        (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) =priceFeed.latestRoundData();
+        return price;
+    }
+
+    function updateAllTokenUris(string memory trend) internal{
+        if(compareStrings("bear", trend)){
+            for(uint i=0;i<_tokenIdCounter.current();i++){
+                _setTokenURI(i, bearUrisIpfs[0]);
+            }
+
+        }else{
+            for(uint i=0;i<_tokenIdCounter.current();i++){
+                _setTokenURI(i, bullUrisIpfs[0]);
+            }
+        }
+        emit tokensUpdated(trend);
+    }
+
+    function setInterval(uint newInterval) public onlyOwner{
+        interval = newInterval;
+    }
+
+    function setPriceFeed(address newFeed) public onlyOwner{
+        priceFeed = AggregatorV3Interface(newFeed);
+    }
+
+    //Helpers
+
+    function compareStrings(string memory a, string memory b) internal pure returns(bool){
+        return(keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b))) ; 
+    }
+
 
     // The following functions are overrides required by Solidity.
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
         super._burn(tokenId);
     }
 
