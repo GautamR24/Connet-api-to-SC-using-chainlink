a57f67682071e41f174791af605ce705896cf8c3
GautamR24 <rawatgautam24@protonmail.com>
2022-11-13 19:34:51 +0530

    nothing much

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,14 +16,7 @@ module.exports = {
       }
     ],
   },
-  // networks: {
-  //   goerli: {
-  //     url: "https://eth-goerli.g.alchemy.com/v2/Pgda-GOHH0CHAfBH0jCmqfhvbwitCPp_",
-  //     accounts: ["1ee7a3c28042920680fec6b7a45cbe20d9d891a4c7eaf8de121b694e06e8a05b"]
-  //   }
-  // },
-  // etherscan: {
-  //   apiKey: "BUVFYBCKRN3PB5QC9IIUU68DRTBD6R7WMS"
+  
 
   // },
   networks: {
